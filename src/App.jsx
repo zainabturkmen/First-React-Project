9da04e9ade3cb5c55d396a6b2c12c8c67eeb67a7
Zaianb <132147692+sampleRID@users.noreply.github.com>
2023-10-28 20:44:16 +0430

    making change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
+
 export default function App() {
     return (
         <div className="container">
